0281d66c155560e1c530e10c7db5f245e3893132
pravesh dav <humantorch1932@gmail.com>
2024-06-17 20:45:17 +0530

    deleting usermodel

--- routes/user.js
+++ routes/user.js
@@ -149,42 +149,42 @@ router.post("/update", async (req, res) => {
   }
 });
 
-
 //  Delete Account
 
-router.post('/delete', async (req, res)=>{
-  const {email, password} = req.body;
+router.post("/delete", async (req, res) => {
+  const { email, password } = req.body;
 
-  try{
+  try {
     // Find user by email
-  const findEmail = await userModel.findOne({ email });
-  if (!findEmail) {
-    return res.status(404).json({
-      success: false,
-      message: "User is not registered",
-    });
-  }
+    const findEmail = await userModel.findOne({ email });
+    if (!findEmail) {
+      return res.status(404).json({
+        success: false,
+        message: "User is not registered",
+      });
+    }
 
-  // Compare the saved hash with the password submitted by the user
-  const isMatch = await bcrypt.compare(password, findEmail.password);
-  if (!isMatch) {
-    return res.status(401).json({
-      success: false,
-      message: "Invalid email or password",
+    // Compare the saved hash with the password submitted by the user
+    const isMatch = await bcrypt.compare(password, findEmail.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    let deleteUser = await userModel.deleteOne({email});
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted the account",
     });
-  }
-  
-  res.status(200).json({
-    success: true,
-    message: "Successfully deleted the account",
-  });
-  }catch(err){
+  } catch (err) {
     // Catch and handle errors
     res.status(500).json({
       success: false,
       message: "Server Error",
     });
   }
-})
+});
 
 module.exports = router;
