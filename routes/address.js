8af2dbd1a748f7a86986d8564f95909853910082
pravesh dav <humantorch1932@gmail.com>
2024-06-18 17:29:05 +0530

    created addressmodel and pushed the id of it to the user

--- routes/address.js
+++ routes/address.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const addressModel = require("../models/address-model");
+const userModel = require('../models/user-model');
 
 router.post("/create", async (req, res) => {
     let { name, email, contact, pincode, locality, address, state, district, landmark, alternateContact, addressType } = req.body;
-    console.log(name, email, contact, pincode, locality, address, state, district, landmark, alternateContact, addressType);
+    let findUser = await userModel.findOne({email});
+    if (!findUser) {
+        return res.status(404).json({
+          success: false,
+          message: "User is not registered",
+        });
+      }
+      else{
+          const createdAddress = await addressModel.create({
+              name, contact, pincode, locality, address, state, district, landmark, alternateContact, addressType, user: findUser._id
+            })
+            findUser.address.push(createdAddress._id);
+            await findUser.save();
+        }
   res.send("this is message");
 });
 module.exports = router;
