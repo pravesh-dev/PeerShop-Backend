b90e9c4a0317e82f015151c051e8a7f4905aee99
pravesh dav <humantorch1932@gmail.com>
2024-06-19 17:01:48 +0530

    handled errors addresses

--- routes/address.js
+++ routes/address.js
@@ -18,7 +18,8 @@ router.post("/create", async (req, res) => {
     alternateContact,
     addressType,
   } = req.body;
-  let findUser = await userModel.findOne({ email });
+  try{
+    let findUser = await userModel.findOne({ email });
   if (!findUser) {
     return res.status(404).json({
       success: false,
@@ -41,28 +42,56 @@ router.post("/create", async (req, res) => {
     findUser.address.push(createdAddress._id);
     await findUser.save();
   }
-  res.send("this is message");
+  }catch(err){
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: err.message,
+    });
+  }
 });
 
 // Sending all address for the user
 router.post("/data", async (req, res) => {
-  const { email } = req.body;
-  let user = await userModel.findOne({ email }).populate("address");
-  res.send(user.address);
+  try{
+    const { email } = req.body;
+    let user = await userModel.findOne({ email }).populate("address");
+    res.send(user.address);
+  }catch(err){
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: err.message,
+    });
+  }
 });
 
 // Deleting the address
 router.post("/delete", async (req, res) => {
-  const { addressId, userId } = req.body;
-  console.log(addressId, userId);
-  let findAddress = await addressModel.deleteOne({_id: addressId});
-  let findUser = await userModel.findOneAndUpdate(
-    { _id: userId },
-    { $pull: { address: addressId } }
-  );
-  res.status(200).json({
-    success: true,
-    message: "Successfully deleted the account",
-  });
+  try{
+
+    const { addressId, userId } = req.body;
+    console.log(addressId, userId);
+    // deleting the address
+    let findAddress = await addressModel.deleteOne({_id: addressId});
+    
+    // removing the id of deleted address from the user related to it
+    let findUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { address: addressId } }
+    );
+    
+    // sending success response
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted the account",
+    });
+  }catch(err){
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: err.message,
+    });
+  }
 });
 module.exports = router;
